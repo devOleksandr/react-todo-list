4ca92a018df07eb6032c5ac4a9c20b7225726e9e
devOleksandr <55914390+devOleksandr@users.noreply.github.com>
2020-04-01 18:19:11 +0300

    Lesson 41. Add new elements.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import AddItem from "../item-add-form";
 
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-    const todoData = [
-        { label: 'Drink Coffee', important: false, id: 1 },
-        { label: 'Make Awesome App', important: true, id: 2 },
-        { label: 'Have a lunch', important: false, id: 3 }
-    ];
+    maxId = 100;
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    state = {
+        todoDate: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3}
+        ]
+    };
+
+    deleteItem = (id) =>{
+        this.setState(({todoDate}) =>{
+            const idx = todoDate.findIndex((el)=> el.id === id);
+
+            const before = todoDate.slice(0, idx);
+            const after = todoDate.slice(idx+1);
+
+            const newArray = [...before, ...after];
+            return {
+                todoDate: newArray
+            }
+        });
+    };
+
+    addItem = (text) =>{
+      // generate id?
+      const newItem = {
+          label: text,
+          important: false,
+          id: this.maxId++
+      };
+
+      this.setState(({todoDate}) =>{
+          const newArr = [
+              ...todoDate,
+              newItem
+          ];
+          return{
+              todoDate: newArr
+          }
+      });
+        //add element in array?
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3}/>
+                <div className="top-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+
+                <TodoList todos={this.state.todoDate}
+                          onDeleted={this.deleteItem}/>
+                 <div className="add-item__inner">
+                     <AddItem onItemAdded ={this.addItem}/>
+                 </div>
             </div>
-
-            <TodoList todos={todoData}
-            onDeleted={ (id) => console.log(id)}/>
-        </div>
-    );
+        );
+    }
 };
 
-export default App;
