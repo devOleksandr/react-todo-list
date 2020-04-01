bdb03c7629949430d4bffce14e0281ec03005e1c
devOleksandr <55914390+devOleksandr@users.noreply.github.com>
2020-04-01 13:59:13 +0300

    Lesson 36. Change state.

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -12,16 +12,37 @@ class TodoListItem extends React.Component{
     //         console.log(`Done: ${this.props.label}`)
     //     }
     // }
+    // Використовуючи "Конструктор"
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         done: false
+    //     }
+    // }
+
+    state = {
+      done: false
+    };
+
 
     //Використовуючи "Поля классов"
     onLabelClick = () =>{
-        console.log(`Done: ${this.props.label}`)
+        this.setState({
+            done: true
+        });
     };
 
 
     render(){
 
         const { label, important = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'todo-list-item';
+
+        if (done){
+            classNames += ' done';
+        }
 
         const style = {
             color: important ? 'steelblue' : 'black',
@@ -29,7 +50,7 @@ class TodoListItem extends React.Component{
         };
 
         return (
-            <span className="todo-list-item">
+            <span className={classNames}>
               <span
                   className="todo-list-item-label"
                   style={style}
